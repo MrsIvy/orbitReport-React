de52096cb0f3fd8912f6cb7059aa1103379e2a2e
LaTanya Ivy <latanyaivy@gmail.com>
2024-09-02 20:48:52 -0500

    step 4 completed

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,38 @@
+import satData from "./satData";
+import React, { useState } from 'react';
+import Buttons from './Buttons';
+import Table from './Table';
+import Banner from './Banner';
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+ 
+
+function App() {
+  const [sat, setSat] = useState(satData);
+  const [displaySats, setDisplaySats] = useState(satData);
+  const filterByType = (currentType) => {
+    const filteredSats = satData.filter(sat => sat.type === currentType);
+    setDisplaySats(filteredSats);
+  };
+  function Buttons({ filterByType, setSat, displaySats }) {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <button>{sat} Orbit</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick ={() => filterByType(sat.type)} key={id}>
+          {sat.name} Orbit
+        </button>
+        )
+      }
+    )}
+      {/* <button>All Orbits</button> */}
+      <button onClick={() => setSat(satData)}>
+        Reset Satellites
+      </button>
+    
     </div>
   );
 };
+}
 
 export default Buttons;

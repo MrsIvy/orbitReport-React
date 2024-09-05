f6f18e8ce9c0f1fa40d963b82b711ba162073305
LaTanya Ivy <latanyaivy@gmail.com>
2024-09-04 22:04:29 -0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
+import { useState } from "react";// 
+import satData from "./components/satData";
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-import Banner from "./components/Banner";
-import { useState } from "react";
-import satData from "./components/satData";
+
+
 function App() {
   const [sat, setSat] = useState(satData);
   
-  const displaySats =[...newSet(satData.map((data) => data.orbitType
+  const displaySats =[...new Set(satData.map((data) => data.orbitType
   ))];
   
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+setSat(displaySats)
+  };
   return (
     <div>
       <Banner />
-      <Buttons
-      filterByType = {filterByType}
-      setSat = {setSat}
-      displaySats = {displaySats}
+       <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats}
       />
-      <Table sat = {sat}/>
+      <Table sat={sat}/> 
       </div>
   );
 }
-  const filterByType = (currentType) => {
-satData.filter((newSatDisplay) => {
-  return newSatDisplay.orbitType === currentType;
-});
-setSat(displaySats);
-};
+
   
 
 export default App;
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,38 +1,24 @@
 import satData from "./satData";
-import React, { useState } from 'react';
-import Buttons from './Buttons';
-import Table from './Table';
-import Banner from './Banner';
-const Buttons = ({ filterByType, setSat, displaySats }) => {
- 
+import "./styling.css"
 
-function App() {
-  const [sat, setSat] = useState(satData);
-  const [displaySats, setDisplaySats] = useState(satData);
-  const filterByType = (currentType) => {
-    const filteredSats = satData.filter(sat => sat.type === currentType);
-    setDisplaySats(filteredSats);
-  };
-  function Buttons({ filterByType, setSat, displaySats }) {
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
-    <div>
-      <button>{sat} Orbit</button>
-      {displaySats.map((sat, id) => {
+  <div>
+    {
+      displaySats.map((sat, id) => {
         return (
-          <button onClick ={() => filterByType(sat.type)} key={id}>
-          {sat.name} Orbit
-        </button>
-        )
-      }
-    )}
-      {/* <button>All Orbits</button> */}
-      <button onClick={() => setSat(satData)}>
-        Reset Satellites
-      </button>
-    
-    </div>
-  );
-};
+          <button onClick={()=>filterByType(sat)} key={id}>
+          {sat} Orbit
+          </button> 
+          )
+      })
+    }
+    <button onClick={()=>setSat(satData)}> 
+    All Orbits
+    </button>
+
+  </div>
+  )
 }
 
 export default Buttons;
